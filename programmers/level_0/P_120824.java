7ccf1fff84010c240023151a2f9b470528c93c95
kangwongu <kwg0527@naver.com>
2022-10-25 23:39:23 +0900

    짝수 홀수 개수

--- programmers/level_0/P_120824.java
+++ programmers/level_0/P_120824.java
@@ -1,30 +1,33 @@
-///**
-// * 나의 풀이
-// * while문을 통해 양꼬치 주문 수가 10 이상이면 k--했다.
-// * 이렇게 양꼬치 값과 음료수 값을 구했다
-// */
-//class Solution {
-//    public int solution(int n, int k) {
-//        int answer = 0;
-//        int nPrice = n*12000;
-//        while (n >= 10) {
-//            k--;
-//            n -= 10;
-//        }
-//        int kPrice = k*2000;
-//        answer = nPrice + kPrice;
-//
-//        return answer;
-//    }
-//}
-//
-///**
-// * 다른 사람 풀이
-// * 간단하게 총 가격을 구한 후, 양꼬치 주문 수가 10 이상이면 추가되는 음료수 값을 구해서 빼줬다.
-// * 훨씬 간결하게 해결할 수 있었다
-// */
-//class Solution {
-//    public int solution(int n, int k) {
-//        return n * 12000 + k * 2000 - (n / 10 * 2000);
-//    }
-//}
+/**
+ * 나의 풀이
+ */
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int jjak = 0;
+        int hol = 0;
+        for (int num : num_list) {
+            if (num % 2 == 0) jjak++;
+            else hol++;
+        }
+        answer[0] = jjak;
+        answer[1] = hol;
+        return answer;
+    }
+}
+
+/**
+ * 다른 사람 풀이
+ * for문에서 짝수 홀수를 구하고 배열에 값을 넣는 것 까지 한번에 처리했다.
+ * 훨씬 간결하네
+ */
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
+
+        return answer;
+    }
+}
