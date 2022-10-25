f91d61665358229b9b97df3c8f4fab219147f67d
kangwongu <kwg0527@naver.com>
2022-10-25 23:36:17 +0900

    피자 나눠 먹기 (3)

--- programmers/level_0/P_120816.java
+++ programmers/level_0/P_120816.java
@@ -1,30 +1,31 @@
-///**
-// * 나의 풀이
-// * while문을 통해 양꼬치 주문 수가 10 이상이면 k--했다.
-// * 이렇게 양꼬치 값과 음료수 값을 구했다
-// */
-//class Solution {
-//    public int solution(int n, int k) {
-//        int answer = 0;
-//        int nPrice = n*12000;
-//        while (n >= 10) {
-//            k--;
-//            n -= 10;
-//        }
-//        int kPrice = k*2000;
-//        answer = nPrice + kPrice;
-//
-//        return answer;
-//    }
-//}
-//
-///**
-// * 다른 사람 풀이
-// * 간단하게 총 가격을 구한 후, 양꼬치 주문 수가 10 이상이면 추가되는 음료수 값을 구해서 빼줬다.
-// * 훨씬 간결하게 해결할 수 있었다
-// */
-//class Solution {
-//    public int solution(int n, int k) {
-//        return n * 12000 + k * 2000 - (n / 10 * 2000);
-//    }
-//}
+/**
+ * 나의 풀이
+ * 생각보다 어려워서 시간을 많이 잡아먹었다
+ * 무한루프를 활용해 조건을 만족하면 break하는 방식으로 구현했다.
+ */
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        int target = slice;
+        while (true) {
+            answer++;
+            if (target >= n) break;
+            target += slice;
+        }
+        return answer;
+    }
+}
+
+/**
+ * 다른 사람 풀이
+ * if-else 문으로 간결하게도 풀 수 있었다.
+ * 왜 이런 생각은 못했지..
+ */
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if (n%slice==0) answer = n/slice;
+        else answer = (n/slice) + 1;
+        return answer;
+    }
+}
